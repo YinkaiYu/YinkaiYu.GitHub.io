15f71a9f8d50b28b63c6298681932b6185ad00e2
YinkaiYu <104674783+YinkaiYu@users.noreply.github.com>
2022-07-08 01:40:38 +0800

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -3,6 +3,6 @@ document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
 document.writeln("        		<li><a href=\"scholar.html\">学术</a></li>");
 document.writeln("        		<li><a href=\"life.html\">生活</a></li>");
-document.writeln("        		<li><a href=\"scholar.html\">共享</a></li>");
+document.writeln("        		<li><a href=\"share.html\">共享</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
