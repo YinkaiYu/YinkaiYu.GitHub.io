6149f15c067d18d578ab61beecd56a600861eb7c
Yin-Kai Yu <yuyinkai@iphy.ac.cn>
2025-11-22 05:12:52 +0800

    添加英文导航栏

---
+++ en/nav.js
@@ -0,0 +1,8 @@
+document.writeln("		<nav>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"../en/index.html\">Home</a></li>");
+document.writeln("        		<li><a href=\"scholar.html\">Scholar</a></li>");
+document.writeln("        		<li><a href=\"life.html\">Life</a></li>");
+document.writeln("        		<li><a href=\"share.html\">Share</a></li>");
+document.writeln("    		</ul>");
+document.writeln("		</nav>");
